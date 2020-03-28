d200b3377d963b971e131d05c955f6a0e4c91ef4
Nikola <40897274+niksy111@users.noreply.github.com>
2020-03-28 16:00:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="LiteTouch 5000LC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/patmann03/pylitetouch",
+    url="https://github.com/niksy111/pylitetouch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
